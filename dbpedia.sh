cfef453be93ce98aae4cc7260f771f562641b6d0
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-02-22 15:13:01 +0100

    add dbpedia merge script

---
+++ dbpedia.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# save directories
+current_dir=$(pwd)
+dbpedia_dir=$1
+
+# decompress archives
+cd $dbpedia_dir
+mkdir archives
+for file in *.bz2; do
+    echo "Decompressing $file ..."
+    bzip2 -dk file
+    mv file archives
+done
+
+# remove first and last line of each file
+for file in *.ttl; do
+    echo "Cleaning $file ..."
+    sed '1d; $d' $file > tmp && mv tmp $file
+done
+
+# merge files
+result_file="dbpedia.ttl"
+> $result_file
+for file in *.ttl; do
+    echo "Appending $file ..."
+    cat $file >> $result_file
+done
+echo "Merged files into $result_file"
+
+# return to original directory
+cd current_dir
