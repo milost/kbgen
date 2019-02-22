f45256b06c5ec253db9839e0c3d90b7bd22c6faf
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-02-22 15:23:59 +0100

    better script output

--- dbpedia.sh
+++ dbpedia.sh
@@ -8,23 +8,22 @@ dbpedia_dir=$1
 cd $dbpedia_dir
 mkdir archives
 for file in *.bz2; do
-    echo "Decompressing $file ..."
+    echo "Decompressing $file"
     bzip2 -dk $file
     mv $file archives
 done
 
 # remove first and last line of each file
 for file in *.ttl; do
-    echo "Cleaning $file ..."
+    echo "Cleaning $file"
     sed '1d; $d' $file > tmp && mv tmp $file
-    rm tmp
 done
 
 # merge files
 result_file="dbpedia.ttl"
 > $result_file
 for file in *.ttl; do
-    echo "Appending $file ..."
+    echo "Appending $file to $result_file"
     cat $file >> $result_file
 done
 echo "Merged files into $result_file"
