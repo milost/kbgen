db632db5a53d8ea001217be498a73ed9b3a90d92
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-03-07 17:42:34 +0100

    add random breaking of fules

--- kbgen/rules/realworld_rule.py
+++ kbgen/rules/realworld_rule.py
@@ -242,6 +242,33 @@ class RealWorldRule(object):
         query = self.full_query_pattern(include_conclusion=True)
         print(f"Query for positive facts: {query}")
         result = list(graph.query(query))
+        # TODO
+
+    def break_randomly(self, graph: Graph, break_chance: float):
+        query = self.full_query_pattern(include_conclusion=True)
+        print(f"Query for {self}: {query}")
+        result = list(graph.query(query))
+
+        positive_facts = set(result)
+        all_facts = list(graph.query(self.full_query_pattern()))
+
+        # how many facts are true (in the ground truth)
+        correctness_ratio = len(positive_facts) / len(all_facts)
+        # how many facts we break as noise
+        brokenness_ratio = 0
+        facts_to_correctness = {self._produce_fact(fact[0], fact[1]): fact in positive_facts for fact in all_facts}
+
+        for person, film in tqdm(result):
+            number = random.random()
+            if number < break_chance:
+                fact = self._produce_fact(person, film)
+                graph.remove(fact)
+                brokenness_ratio += 1
+
+        brokenness_ratio /= len(all_facts)
+        oracle_data = [facts_to_correctness, correctness_ratio, brokenness_ratio]
+        return graph, oracle_data
+
 
     def break_by_birth_date(self,
                             graph: Graph,
@@ -249,7 +276,6 @@ class RealWorldRule(object):
                             break_chance: float,
                             threshold: datetime.date,
                             less_than: bool = True) -> Tuple[Graph, list]:
-
         query = self.full_query_pattern(include_conclusion=True)
         print(f"Query for {self}: {query}")
         result = list(graph.query(query))
--- yago_mangling.py
+++ yago_mangling.py
@@ -1,6 +1,7 @@
 import datetime
 import pickle
 from pathlib import Path
+from typing import Tuple
 
 from rdflib import Graph, URIRef, Literal
 from argparse import ArgumentParser, Namespace
@@ -15,26 +16,76 @@ def cli_args() -> Namespace:
     parser = ArgumentParser(description="Mangle a yago graph")
     parser.add_argument("input", type=str, default=None, help="path to the binary graph file")
     parser.add_argument("-r", "--rules_path", type=str, help="the rule file containing the amie rules")
+    parser.add_argument("--random",  dest='random', action='store_true', help="set if the rules should be broken at "
+                                                                              "random")
     return parser.parse_args()
 
 
-def save_mangled_graph(graph: Graph, oracle: RealWorldOracle, input_file: Path, filter_literals: bool = True):
-    oracle_output = f"{input_file.parent}/{input_file.stem}_mangled_oracle.json"
+def save_mangled_graph(graph: Graph,
+                       oracle: RealWorldOracle,
+                       input_file: Path,
+                       random: bool,
+                       filter_literals: bool = True):
+    suffix = "mangled_random" if random else "mangled_systematic"
+
+    oracle_output = f"{input_file.parent}/{input_file.stem}_{suffix}_oracle.json"
     print(f"Saving graph to {oracle_output}")
     oracle.to_json(open(oracle_output, "w"))
 
-    pickle_output = f"{input_file.parent}/{input_file.stem}_mangled{input_file.suffix}"
+    pickle_output = f"{input_file.parent}/{input_file.stem}{suffix}{input_file.suffix}"
     print(f"Saving graph to {pickle_output}")
     with open(pickle_output, "wb") as graph_file:
         pickle.dump(graph, graph_file, protocol=pickle.HIGHEST_PROTOCOL)
 
-    tsv_output = f"{input_file.parent}/{input_file.stem}_mangled.tsv"
+    tsv_output = f"{input_file.parent}/{input_file.stem}_{suffix}.tsv"
     print(f"Saving graph to {tsv_output}")
     with open(tsv_output, "w") as file:
         for subject, predicate, object in tqdm(graph):
             if not (isinstance(object, Literal) and filter_literals):
                 line = str(subject) + "\t" + str(predicate) + "\t" + str(object)
                 file.write(f"{line}\n")
+
+
+def break_by_date(graph: Graph,
+                  rules: RealWorldRuleSet,
+                  threshold: datetime.date,
+                  break_chance: float) -> Tuple[Graph, RealWorldOracle]:
+    oracle_facts_correctness = {}
+    oracle_facts_correctness_ratio = {}
+    oracle_facts_brokenness_ratio = {}
+    for rule in rules.rules:
+        oracle_facts_correctness[rule] = {}
+        graph, oracle_data = rule.break_by_birth_date(graph,
+                                                      birth_date_relation=URIRef("wasBornOnDate"),
+                                                      break_chance=break_chance,
+                                                      threshold=threshold)
+        facts_correctness, correctness_ratio, brokenness_ratio = oracle_data
+        oracle_facts_correctness[rule] = facts_correctness
+        oracle_facts_correctness_ratio[rule] = correctness_ratio
+        oracle_facts_brokenness_ratio[rule] = brokenness_ratio
+
+    oracle = RealWorldOracle(facts_to_correctness=oracle_facts_correctness,
+                             rules_to_correctness_ratio=oracle_facts_correctness_ratio,
+                             rules_to_brokenness_ratio=oracle_facts_brokenness_ratio)
+    return graph, oracle
+
+
+def break_randomly(graph: Graph, rules: RealWorldRuleSet, break_chance: float) -> Tuple[Graph, RealWorldOracle]:
+    oracle_facts_correctness = {}
+    oracle_facts_correctness_ratio = {}
+    oracle_facts_brokenness_ratio = {}
+    for rule in rules.rules:
+        oracle_facts_correctness[rule] = {}
+        graph, oracle_data = rule.break_randomly(graph, break_chance=break_chance)
+        facts_correctness, correctness_ratio, brokenness_ratio = oracle_data
+        oracle_facts_correctness[rule] = facts_correctness
+        oracle_facts_correctness_ratio[rule] = correctness_ratio
+        oracle_facts_brokenness_ratio[rule] = brokenness_ratio
+
+    oracle = RealWorldOracle(facts_to_correctness=oracle_facts_correctness,
+                             rules_to_correctness_ratio=oracle_facts_correctness_ratio,
+                             rules_to_brokenness_ratio=oracle_facts_brokenness_ratio)
+    return graph, oracle
 
 
 def main():
@@ -50,26 +101,13 @@ def main():
     print(f"Loading rules from {args.rules_path}")
     rules = RealWorldRuleSet.parse_amie(args.rules_path)
 
-    oracle_facts_correctness = {}
-    oracle_facts_correctness_ratio = {}
-    oracle_facts_brokenness_ratio = {}
-    for rule in rules.rules:
-        oracle_facts_correctness[rule] = {}
+    if args.random:
+        graph, oracle = break_randomly(graph, rules, break_chance=0.3)
+    else:
         threshold = datetime.date(year=1950, month=1, day=1)
-        graph, oracle_data = rule.break_by_birth_date(graph,
-                                                      birth_date_relation=URIRef("wasBornOnDate"),
-                                                      break_chance=0.7,
-                                                      threshold=threshold)
-        facts_correctness, correctness_ratio, brokenness_ratio = oracle_data
-        oracle_facts_correctness[rule] = facts_correctness
-        oracle_facts_correctness_ratio[rule] = correctness_ratio
-        oracle_facts_brokenness_ratio[rule] = brokenness_ratio
+        graph, oracle = break_by_date(graph, rules, threshold=threshold, break_chance=0.7)
 
-    oracle = RealWorldOracle(facts_to_correctness=oracle_facts_correctness,
-                             rules_to_correctness_ratio=oracle_facts_correctness_ratio,
-                             rules_to_brokenness_ratio=oracle_facts_brokenness_ratio)
-
-    save_mangled_graph(graph, oracle, input_path)
+    save_mangled_graph(graph, oracle, input_path, random=args.random)
 
 
 if __name__ == '__main__':
