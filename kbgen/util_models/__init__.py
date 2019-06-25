fc7da4b1a88b2e32a36adfb070bfb6ce981e8fd5
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-06-25 15:52:34 +0200

    remove yago imports

--- kbgen/util_models/__init__.py
+++ kbgen/util_models/__init__.py
@@ -3,6 +3,5 @@ from .uri_relation import URIRelation
 from .uri_type import URIType
 from .multi_type import MultiType
 from .oracle import Oracle
-from .realworld_oracle import RealWorldOracle
 
-__all__ = ["URIEntity", "URIRelation", "URIType", "MultiType", "Oracle", "RealWorldOracle"]
+__all__ = ["URIEntity", "URIRelation", "URIType", "MultiType", "Oracle"]
